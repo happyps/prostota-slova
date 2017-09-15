6e00173778d20f3d56aea0186dfc5ee522ac8f8b
Sergei Pechenkin <seregapgh@gmail.com>
2017-09-15 08:28:57 +0300

    Add firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,23 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import {NgPipesModule} from 'ngx-pipes';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB5KFP6t_kSRiiOCg6QwHum19BLkuuXG30",
+  authDomain: "prostota-slova.firebaseapp.com",
+  databaseURL: "https://prostota-slova.firebaseio.com",
+  projectId: "prostota-slova",
+  storageBucket: "prostota-slova.appspot.com",
+  messagingSenderId: "613958240116"
+};
 
 @NgModule({
   declarations: [
@@ -14,6 +29,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgPipesModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,69 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items: FirebaseListObservable<any[]>;
+  state: firebase.User;
+  current: string;
+  
+  constructor(
+    public navCtrl: NavController,
+    public auth: AngularFireAuth,
+    db: AngularFireDatabase) {
+    this.auth.authState.subscribe(state => {
+      console.log('Auth state updated');
+      console.log(state);
+      this.state = state;
+      if (state) {
+        this.items = db.list('/sample');
+      } else {
+        this.items = null;
+      }
+    });
+  }
+  login() {
+    console.log('login');
+    this.auth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    console.log('logout');
+    this.auth.auth.signOut();
+    this.state = null;
+  }
+  addWord(word) {
+    this.items.push(word);
+  }
+  removeWord(word) {
+    console.log(`remove ${word}`);
+    let item = null;
+    this.items.forEach(a => a.forEach(v => {
+      console.log(v);
+      if (v.$value === word) {
+        console.log(`found ${word}`);
+        item = v;
+      }
+    }));
+    if (item) {
+      console.log(`remove ${item}`);
+      this.items.remove(item);
+    }
+  }
+  onKeyPress(event) {
+    if (event.keyCode === 13 && this.current && this.current.length) {
+      if (this.current.indexOf('-') === 0) {
+        this.removeWord(this.current.substr(1));
+      } else {
+        this.addWord(this.current);
+      }
+      this.current = '';
+    }
   }
 
 }
